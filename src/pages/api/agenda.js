8de8574f72d3e919b2ba2fb66b9f4273e7b6b0a6
Daniel Rezende <daniel.zende@gmail.com>
2021-04-16 15:21:17 -0300

    Criada a rota api/agenda

--- src/pages/api/agenda.js
+++ src/pages/api/agenda.js
@@ -1,5 +1,29 @@
+import { firebaseServer } from '../../config/firebase/server';
 
-export default async (req, res) => {
-  console.log(req.query.date)
-  res.status(200).json({ date: req.query.date})
+const db = firebaseServer.firestore()
+const agenda = db.collection('agenda')
+
+
+export default async (request, response) => {
+  const [, token] = request.headers.authorization.split(' ')
+ 
+  if (!token) {
+    return response.status(401)
+  }
+
+  try {
+    const { user_id } = await firebaseServer.auth().verifyIdToken(token)
+
+    const data = await agenda
+        .where('userId', '==', user_id)
+        .where('day', '==', request.query.date)
+        .get()
+
+    return response.status(200).json(data.docs)
+    
+  } catch (error) {
+    console.log('FIREBASE ERROR:', error)
+    return response.status(401)
+  }
+
 }
