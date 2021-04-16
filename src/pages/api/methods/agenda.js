2923eca9713450018508e4ac5c9d927c93ed628d
Daniel Rezende <daniel.zende@gmail.com>
2021-04-16 12:40:28 -0300

    feat: Inclusão do token na requisição

--- src/pages/api/methods/agenda.js
+++ src/pages/api/methods/agenda.js
@@ -1,10 +1,14 @@
 import axios from "axios";
-import { format } from 'date-fns'
+import { format } from 'date-fns';
+
+import { getToken } from '../../../config/firebase/client';
 
 export const getAgenda = async (day) => {
-  const { data } = await axios({
+  const token = await getToken();
+
+  return axios({
     method: 'GET',
-    // headers: { Authorization: `Bearer ${token}` },
+    headers: { Authorization: `Bearer ${token}` },
     url: '/api/agenda',
     params: { date: format(day, 'yyyy-MM-dd'), }
   })
